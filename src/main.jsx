7fb3186ab31c32e0204716e0aa86d4d5bdabc0f2
JoaldoSanth <joaldosant3@gmail.com>
2024-05-19 02:42:10 -0300

    projetos no react

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Conversor_de_moedas from './App.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Conversor_de_moedas />
+  </React.StrictMode>,
+)
